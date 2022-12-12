85fa9ad3b6f2a6aba0fdb7f359d33171a93d2c92
krakh3ad <mainakaranja2001@gmail.com>
2022-12-12 08:46:18 +0300

    First Commit

---
+++ woodridge/core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about-us', views.about, name='about-us'),
+    path('gallery', views.gallery, name='gallery'),
+    path('news', views.news, name='news'),
+    path('admissions', views.admissions, name='admissions'),
+    path('enquiries', views.enquiries, name='enquiries'),
+
+
+
+]
---
+++ woodridge/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about-us.html')
+
+def gallery(request):
+    return render(request, 'core/gallery.html')
+
+def news(request):
+    return render(request, 'core/news.html')
+
+def admissions(request):
+    return render(request, 'core/admissions.html')
+
+def enquiries(request):
+    return render(request, 'core/enquiries.html')
