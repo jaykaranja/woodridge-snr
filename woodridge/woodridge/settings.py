511091dce324f2588707dab79e5deb5011c6479f
krakh3ad <mainakaranja2001@gmail.com>
2022-12-17 17:48:13 +0300

    Made some security changes

--- woodridge/woodridge/settings.py
+++ woodridge/woodridge/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8+%s8t-f&!)9&qn+@6%lk3o10xi3)_f$_hcn#=voiu9p80i(cz'
+
+SECRET_KEY = config("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
